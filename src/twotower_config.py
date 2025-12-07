62d7b5c970f75c23538a92f584f5dd9b25a6070d
Jing Liu <jliu3146@gatech.edu>
2025-12-06 23:58:19 -0800

    two tower config update

--- src/twotower_config.py
+++ src/twotower_config.py
@@ -2,19 +2,13 @@ import yaml
 import os
 from typing import Any, Dict, List
 
-# 项目根目录
 ROOT = os.path.dirname(os.path.dirname(__file__))
 
-# YAML 配置路径
-CONFIG_PATH = os.path.join(ROOT, "configs", "config.yaml")
+CONFIG_PATH = os.path.join(ROOT, "configs", "twotower_config.yaml")
 
-# 加载 YAML
+# load YAML
 with open(CONFIG_PATH, "r") as f:
     _raw_config: Dict[str, Any] = yaml.safe_load(f)
-
-# preprocess
-DATA_RAW_PATH: str = _raw_config["preprocess"]["DATA_RAW_PATH"]
-PROCESSED_DIR: str = _raw_config["preprocess"]["PROCESSED_DIR"]
 
 # train
 BATCH_SIZE: int = _raw_config["train"]["batch_size"]
